118b7814245daddc3de01ac37391b38b13a3dee1
BrunoCaputo <brunopc.dev@gmail.com>
2025-04-14 22:42:24 -0300

    feat: Using style components

---
+++ src/screens/Groups/styles.ts
@@ -0,0 +1,15 @@
+import styled from 'styled-components/native'
+
+const Container = styled.View`
+  flex: 1;
+  background-color: #000;
+  align-items: center;
+  justify-content: center;
+`
+
+const Title = styled.Text`
+  color: #fff;
+  font-size: 32px;
+`
+
+export { Container, Title }
