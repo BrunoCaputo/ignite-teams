cc28ea7fba0fe461a116806772505ca743d36e99
BrunoCaputo <brunopc.dev@gmail.com>
2025-04-16 21:44:37 -0300

    feat: Theme config

--- src/screens/Groups/styles.ts
+++ src/screens/Groups/styles.ts
@@ -2,14 +2,8 @@ import styled from 'styled-components/native'
 
 const Container = styled.View`
   flex: 1;
-  background-color: #000;
-  align-items: center;
-  justify-content: center;
+  background-color: ${({ theme }) => theme.COLORS.GRAY_600};
+  padding: 1.5rem;
 `
 
-const Title = styled.Text`
-  color: #fff;
-  font-size: 32px;
-`
-
-export { Container, Title }
+export { Container }
