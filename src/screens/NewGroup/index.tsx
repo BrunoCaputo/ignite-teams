d59259fd3ceabe8bf125bb1daf235d8958cb437e
BrunoCaputo <brunopc.dev@gmail.com>
2025-04-17 22:01:51 -0300

    feat: Input component

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -1,10 +1,14 @@
 import { Button } from '@components/Button'
 import { Header } from '@components/Header'
 import { Highlight } from '@components/Highlight'
+import { Input } from '@components/Input'
+import { useTheme } from 'styled-components/native'
 
 import { Container, Content, Icon } from './styles'
 
 export function NewGroup() {
+  const theme = useTheme()
+
   return (
     <Container>
       <Header showBackButton />
@@ -17,7 +21,12 @@ export function NewGroup() {
           subtitle="crie a turma para adicionar as pessoas"
         />
 
-        <Button title="Criar" />
+        <Input
+          placeholder="Nome da turma"
+          placeholderTextColor={theme.COLORS.GRAY_300}
+        />
+
+        <Button title="Criar" style={{ marginTop: 20 }} />
       </Content>
     </Container>
   )
