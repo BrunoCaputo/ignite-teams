f0c683598422fb7e55841e1a8b2955ac3df4aecf
BrunoCaputo <brunopc.dev@gmail.com>
2025-04-25 20:34:54 -0300

    feat: Screens safe area view

--- src/screens/Groups/styles.ts
+++ src/screens/Groups/styles.ts
@@ -1,6 +1,7 @@
+import { SafeAreaView } from 'react-native-safe-area-context'
 import styled from 'styled-components/native'
 
-const Container = styled.View`
+const Container = styled(SafeAreaView)`
   flex: 1;
   background-color: ${({ theme }) => theme.COLORS.GRAY_600};
   padding: 24px;
--- src/screens/NewGroup/styles.ts
+++ src/screens/NewGroup/styles.ts
@@ -1,7 +1,8 @@
 import { UsersThree } from 'phosphor-react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
 import styled from 'styled-components/native'
 
-const Container = styled.View`
+const Container = styled(SafeAreaView)`
   flex: 1;
   background-color: ${({ theme }) => theme.COLORS.GRAY_600};
   padding: 24px;
