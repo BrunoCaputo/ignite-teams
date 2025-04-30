7db99c06d7b0e9cf3e73ae66fb90801c711d96d9
BrunoCaputo <brunopc.dev@gmail.com>
2025-04-29 23:11:09 -0300

    feat: Fetch groups on Groups screen focus

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -3,8 +3,9 @@ import { GroupCard } from '@components/GroupCard'
 import { Header } from '@components/Header'
 import { Highlight } from '@components/Highlight'
 import { ListEmpty } from '@components/ListEmpty'
-import { useNavigation } from '@react-navigation/native'
-import { useState } from 'react'
+import { useFocusEffect, useNavigation } from '@react-navigation/native'
+import { getAllGroups } from '@storage/group/getAllGroups'
+import { useCallback, useState } from 'react'
 import { FlatList } from 'react-native'
 
 import { Container } from './styles'
@@ -16,6 +17,21 @@ export function Groups() {
   const handleNewGroup = () => {
     navigation.navigate('new')
   }
+
+  async function fetchGroups() {
+    try {
+      const data = await getAllGroups()
+      setGroups(data)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  useFocusEffect(
+    useCallback(() => {
+      fetchGroups()
+    }, []),
+  )
 
   return (
     <Container>
